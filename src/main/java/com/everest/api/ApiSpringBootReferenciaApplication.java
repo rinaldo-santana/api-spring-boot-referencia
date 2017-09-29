e839ca10b3f66ab99ba9d6d6717b7ac7470771dc
rinaldo-santana <rinaldo.rms@gmail.com>
2017-09-29 20:38:03 -0300

    commit inicial do projeto.

---
+++ src/main/java/com/everest/api/ApiSpringBootReferenciaApplication.java
@@ -0,0 +1,13 @@
+package com.everest.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringBootReferenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringBootReferenciaApplication.class, args);
+		System.out.println(" >>>> Projeto do curso API RESTful com Spring Boot e Java8");
+	}
+}
